97140c420446331fe98923255bc8ac74ddd4f970
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2023-06-27 00:26:34 +0530

    adding backend

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const DB_CONNECT = "mongodb://localhost:27017/todo"
+
+export const JWT_TOKEN_SECRET_KEY = "jaimahismationtheway" 
+export const statusCode = {
+    SUCCESS:200,
+    VALIDATION_ERROR:201,
+    UNPROCESSABLE_ENTITY:202,
+    AUTH_ERROR:203
+}
